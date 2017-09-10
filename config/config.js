e321fa20d44a66e8d798e90643024fb6f9faf58d
Deepak Brahmania <deepak.brahmania@timeinc.com>
2017-09-11 03:12:14 +0530

    tokn generated3

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 function getToken() {
     return {
-        TOKEN:'d18f10abe8ef6b1a91b2d2d728a8cf8041ef2167'
+        TOKEN:'42666af66492279fde9f6bc8752463208365685b'
     };
 }
 module.exports = {
